da62d6351a792cf7b0b90414f5a743c5d7563282
Bernardo Henrique Lander Leefmans <bernardo.lander@farmatodo.com>
2025-07-25 20:30:30 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <DashboardPage>
    
    w

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,7 +18,7 @@ export default function DashboardPage() {
       <div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
         <div className="flex items-center">
           <h1 className="text-3xl font-bold font-headline tracking-tight">
-            Inicio
+            Procesador de Archivos
           </h1>
         </div>
         <FileUploader onUploadComplete={handleUploadComplete} />
