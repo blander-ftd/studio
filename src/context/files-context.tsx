e2bb71cd5d382487cc08ba0df5ec4c50113fb0cb
Bernardo Henrique Lander Leefmans <bernardo.lander@farmatodo.com>
2025-07-29 20:52:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/context/files-context.tsx
+++ src/context/files-context.tsx
@@ -137,8 +137,7 @@ export const FilesProvider = ({ children }: { children: ReactNode }) => {
     });
     
     setFiles(prevFiles => {
-        const existingIds = new Set(prevFiles.map(f => f.id));
-        const newUniqueFiles = filesWithStatus.filter(f => !existingIds.has(f.id));
+        const newUniqueFiles = filesWithStatus.filter(f => !prevFiles.some(pf => pf.id === f.id));
         return [...newUniqueFiles, ...prevFiles];
     });
   }, []);
