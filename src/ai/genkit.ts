29af5ebaab3998f1fa153e9850e2d317d7220f78
BernardoLander <bernardo.lander@farmatodo.com>
2025-08-06 14:12:33 -0400

    firebase deployed :rocket:

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: "AIzaSyBEdwmk2KRdTA_iT9RWpFD8FDHXegJHwuw",
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
